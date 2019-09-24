83336c484dfa4534d7e1fba046171511e15622a9
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-09-24 09:09:21 +0200

    added new endpoint to get persons by QueryString

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const personRoute = require('./routes/person');
+
+app.use(personRoute);
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
---
+++ src/routes/person.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/person', (req, res) => {
+  if (req.query.name) {
+    res.send(`You have requested a person called ${req.query.name}`);
+  } else {
+    res.send('You have requested a person');
+  }
+});
+
+router.get('/person/:name', (req, res) => {
+  res.send(`You have requested a person called ${req.params.name}`);
+});
+
+module.exports = router;
